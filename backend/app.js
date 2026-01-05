d62313f44bf7f000b2943d9121569333959f2dc6
Waqas106 <m.waqas987321@gmail.com>
2026-01-05 12:31:26 +0500

    Upated App file

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,13 @@ import projectRoutes from './routes/projectRoutes.js';
 import taskRoute from './routes/taskRoute.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [
+    "http://localhost:5173",
+    "https://project-management-platform-mocha.vercel.app/"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
